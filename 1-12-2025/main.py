11d273880abf08e4d26e88bd46ecdadce8d8c6f3
Sumukha Rao H <sumukharaoh05@gmail.com>
2025-12-01 13:14:42 +0530

    day1 part2

--- 1-12-2025/main.py
+++ 1-12-2025/main.py
@@ -1,6 +1,6 @@
 import sys
 
-def unlock(data):
+def part1(data):
     count = 0
     cur = 50
     for i in data:
@@ -11,6 +11,24 @@ def unlock(data):
         cur %= 100
         if cur == 0:
             count += 1
+    return count
+
+def part2(data):
+    count = 0
+    cur = 50
+
+    for d, step in data:
+        if d == "R":
+            raw = cur + step
+            if raw >= 100:
+                count += raw // 100
+            cur = raw % 100
+        else:
+            raw = cur - step
+            if raw <= 0:
+                count += (-raw // 100) + (cur != 0)
+            cur = raw % 100
+
     return count
 
 def split_lines(data):
@@ -24,9 +42,11 @@ def main():
     # data now contains all lines from input.txt
     data = split_lines(data)
     # print(data)
-    ans = unlock(data)
+    ans1 = part1(data)
+    ans2 = part2(data)
 
-    print(ans)
+    print("Part1:", ans1)
+    print("Part2:", ans2)
 
 
 if __name__ == "__main__":
